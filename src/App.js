515c03981398319267c1bc2f905b49631133ae5a
senspond20 <senspond@gmail.com>
2020-07-10 05:48:52 +0900

    app edit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div>
       <h1>안녕하세요 방가방가</h1>
+      <h2>Hi hi</h2>
     </div>
   );
 }
