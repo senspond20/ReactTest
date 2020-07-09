de73d9fd4e400da8d603168514ca1615c6aa295e
senspond20 <senspond@gmail.com>
2020-07-10 04:03:51 +0900

    Updates

---
+++ precache-manifest.d5073a5ee2ae335d985bc24cd1532e3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2e858cc63d8b0b24b86c4fdf05c9c38",
+    "url": "/ReactTest/index.html"
+  },
+  {
+    "revision": "d0936390912190f26c81",
+    "url": "/ReactTest/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a1e933889b1a9cb0af47",
+    "url": "/ReactTest/static/js/2.d3b923cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactTest/static/js/2.d3b923cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0936390912190f26c81",
+    "url": "/ReactTest/static/js/main.16f7a822.chunk.js"
+  },
+  {
+    "revision": "21ce99c32b899ffa485f",
+    "url": "/ReactTest/static/js/runtime-main.d8996c47.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactTest/static/media/logo.5d5d9eef.svg"
+  }
+]);
