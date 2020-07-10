8734cca87aacf48033ebe385581de89cff6911ca
senspond20 <senspond@gmail.com>
2020-07-10 21:56:48 +0900

    config

---
+++ config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    title: `Web Lab`,
+    description: `안녕하세요. senshig의 WEB 연구소입니다.`,
+    author: `[senshig]`,
+    introduction: `안녕하세요. senshig의 WEB 연구소입니다.`,
+    siteUrl: `https://senshig-blog.netlify.app/`, // Your blog site url
+    social: {
+      twitter: ``, // Your Twitter account
+      github: ``, // Your GitHub account
+      medium: ``, // Your Medium account
+      facebook: ``, // Your Facebook account
+      linkedin: ``, // Your LinkedIn account
+    },
+    icon: `content/assets/felog.png`, // Add your favicon
+    keywords: [`blog`],
+    // comment: {
+    //   disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    //   utterances: '', // Your repository for archive comment
+    // },
+    configs: {
+      countOfInitialPost: 10, // Config your initial count of post
+    },
+    sponsor: {
+      buyMeACoffeeId: 'senshig',
+    },
+    share: {
+      facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+    },
+    ga: '', // Add your google analytics tranking ID
+  }
+  
